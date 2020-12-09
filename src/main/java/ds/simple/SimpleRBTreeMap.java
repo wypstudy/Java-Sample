5e3bcf5f7c4abe215462c9492067d1e252f8d1ca
wypstudy <wei.yiping@frontsurf.com>
2020-12-09 18:07:59 +0800

    红黑树实现Map完成插入和查找,删除待实现

--- src/main/java/ds/simple/SimpleRBTreeMap.java
+++ src/main/java/ds/simple/SimpleRBTreeMap.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
  * Date  :  2020/12/8
  * Author:  YiPing, Wei
  **/
-public class SimpleRBTreeMap<K, V> {
+public class SimpleRBTreeMap<K extends Comparator<K>, V> {
     private static boolean BLACK = true;
     private static boolean RED = false;
 
@@ -17,11 +17,13 @@ public class SimpleRBTreeMap<K, V> {
         boolean color;
         TreeNode<K, V> parent, left, right;
 
-        TreeNode(K key, V value, TreeNode<K, V> parent) {
-            this.color = BLACK; // 插入节点默认黑色
+        TreeNode(K key, V value, boolean color, TreeNode<K, V> parent, TreeNode<K, V> left, TreeNode<K, V> right) {
             this.key = key;
             this.value = value;
+            this.color = color;
             this.parent = parent;
+            this.left = left;
+            this.right = right;
         }
 
         @Override
@@ -31,17 +33,12 @@ public class SimpleRBTreeMap<K, V> {
     }
 
     TreeNode<K, V> root;
-    Comparator<? super K> cpr;
     int size;
 
     SimpleRBTreeMap() {
         size = 0;
     }
 
-    SimpleRBTreeMap(Comparator<? super K> cpr) {
-        this();
-        this.cpr = cpr;
-    }
 
     public int size() {
         return size;
@@ -53,68 +50,216 @@ public class SimpleRBTreeMap<K, V> {
 
     public void put(K key, V value) {
         int cmp;
-        TreeNode<K, V> p, e;
-        if ((p = root) == null) {
-            root = new TreeNode<>(key, value, null);
+        TreeNode<K, V> p = search(key), e;
+        if (p == null) {
+            // 根节点插入
+            root = new TreeNode<>(key, value, BLACK, null, null, null);
             size++;
         } else {
-            Comparator<? super K> k = cpr != null ? cpr : (Comparator<? super K>) key;
-            e = p;
-            do {
-                p = e;
-                cmp = k.compare(key, e.key);
-                if (cmp < 0) {
-                    e = e.left;
-                } else if (cmp > 0) {
-                    e = e.right;
-                } else {
-                    e.value = value;
-                    return;
-                }
-            } while (e != null);
-            e = new TreeNode<>(key, value, p);
-            if (cmp < 0) {
-                p.left = e;
+            // 叶节点插入
+            Comparator<? super K> k = key;
+            cmp = k.compare(key, p.key);
+            if (cmp == 0) {
+                // 能找到 key 赋值
+                p.value = value;
             } else {
-                p.right = e;
+                // 找不到 key 插入
+                e = new TreeNode<>(key, value, RED, p, null, null);
+                if (cmp < 0) {
+                    p.left = e;
+                } else {
+                    p.right = e;
+                }
+                // 修正成红黑树
+                fixAdd(e);
+                size++;
             }
-            fixAdd(e);
         }
 
     }
 
     public V get(K key) {
-        int cmp;
-        TreeNode<K, V> p = root;
-        Comparator<? super K> k = cpr != null ? cpr : (Comparator<? super K>) key;
-        while (p != null) {
-            cmp = k.compare(key, p.key);
-            if (cmp < 0) {
-                p = p.left;
-            } else if (cmp > 0) {
-                p = p.right;
-            } else {
-                return p.value;
-            }
-        }
+        TreeNode<K, V> x = search(key);
+        if (x != null && key.equals(x.key))
+            return x.value;
         return null;
     }
 
     public V remove(K key) {
-        return null;
+        TreeNode<K, V> x = search(key);
+        V re = null;
+        if (x != null && key.equals(x.key)) {
+            re = x.value;
+            removeNode(x);
+        }
+        return re;
+    }
+
+    private void removeNode(TreeNode<K, V> x) {
+        // TODO 完成节点删除逻辑
+    }
+
+    // 查找 key,找不到时返回可插入节点
+    private TreeNode<K, V> search(K key) {
+        int cmp;
+        TreeNode<K, V> p = root, e = p;
+        Comparator<? super K> k = key;
+        do {
+            p = e;
+            cmp = k.compare(key, e.key);
+            if (cmp < 0) {
+                e = e.left;
+            } else if (cmp > 0) {
+                e = e.right;
+            } else {
+                // 查找到返回
+                return e;
+            }
+        } while (e != null);
+        // 查找不到返回可插入节点
+        return p;
     }
 
     private void fixAdd(TreeNode<K, V> x) {
-        x.color = RED;
-        while (x != null && x!=root && x.parent.color == RED) {
-            if (x.parent == x.parent.parent.left) {
+        TreeNode<K, V> parent, grandparent, uncle;
 
+        // 父节点存在且父节点是红色与当前节点颜色冲突
+        while ((parent = x.parent) != null && isRed(parent)) {
+            grandparent = parent.parent;
+
+            if (parent == grandparent.left) {
+                // 父节点是祖父节点左子节点
+                uncle = grandparent.right;
+
+                // 情况1 叔叔节点是红色
+                if (uncle != null && isRed(uncle)) {
+                    setBlack(uncle);
+                    setBlack(parent);
+                    setRed(grandparent);
+                    x = grandparent;
+                    continue;
+                }
+
+                // 情况2 叔叔节点是黑色,且当前节点是右子节点
+                if (parent.right == x) {
+                    TreeNode<K, V> tmp;
+                    rotateLeft(parent);
+                    tmp = parent;
+                    parent = x;
+                    x = tmp;
+                }
+
+                // 情况3 叔叔节点是黑色,且当前节点是左子节点
+                setBlack(parent);
+                setRed(grandparent);
+                rotateRight(grandparent);
+            } else {
+                // 父节点是祖父节点右子节点
+                uncle = grandparent.left;
+
+                // 情况1 叔叔节点是红色
+                if (uncle != null && isRed(uncle)) {
+                    setBlack(uncle);
+                    setBlack(parent);
+                    setRed(grandparent);
+                    x = grandparent;
+                    continue;
+                }
+
+                // 情况2 叔叔节点是黑色,且当前节点是左孩子
+                if (parent.left == x) {
+                    TreeNode<K, V> tmp;
+                    rotateRight(parent);
+                    tmp = parent;
+                    parent = x;
+                    x = tmp;
+                }
+
+                // 情况3 叔叔节点是黑色,且当前节点是右孩子
+                setBlack(parent);
+                setRed(grandparent);
+                rotateLeft(grandparent);
             }
         }
+        setBlack(root);
     }
 
     private void fixRemove(TreeNode<K, V> x) {
+        // TODO 完成删除后修复红黑树逻辑
+    }
 
+    private boolean isRed(TreeNode<K, V> x) {
+        return x != null && x.color == RED;
+    }
+
+    private boolean isBlack(TreeNode<K, V> x) {
+        return x == null || x.color == BLACK;
+    }
+
+    private void setRed(TreeNode<K, V> x) {
+        x.color = RED;
+    }
+
+    private void setBlack(TreeNode<K, V> x) {
+        x.color = BLACK;
+    }
+
+    /*
+     *       px                   px
+     *      /                    /
+     *     x                    y
+     *    / \      --左旋->     / \
+     *  lx  y                 x   ry
+     *     / \               / \
+     *   ly  ry            lx  ly
+     */
+    private void rotateLeft(TreeNode<K, V> x) {
+        TreeNode<K, V> y = x.right;
+        x.right = y.left;
+        if (y.left != null)
+            y.left.parent = x;
+        y.parent = x.parent;
+        if (x.parent == null) {
+            root = y;
+        } else {
+            if (x.parent.left == x) {
+                x.parent.left = y;
+            } else {
+                x.parent.right = y;
+            }
+        }
+        y.left = x;
+        x.parent = y;
+    }
+
+    /*
+     *       py                   py
+     *      /                    /
+     *     y                    x
+     *    / \      --右旋->     / \
+     *   x  ry                lx  y
+     *  / \                      / \
+     * lx  rx                   rx  ry
+     */
+    private void rotateRight(TreeNode<K, V> y) {
+        TreeNode<K, V> x = y.left;
+        y.left = x.right;
+        if (x.right != null) {
+            x.right.parent = y;
+        }
+        x.parent = y.parent;
+        if (y.parent == null) {
+            root = x;
+        } else {
+            if (y.parent.left == y) {
+                y.parent.left = x;
+            } else {
+                y.parent.right = x;
+            }
+
+        }
+        x.right = y;
+        y.parent = x;
     }
 
 }
